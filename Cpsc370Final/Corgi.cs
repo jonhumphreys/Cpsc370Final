f267b61cc51ae40271ccdd09644516d3f3c57148
Kalin Richardson <kalrichardson@chapman.edu>
2025-01-28 21:36:48 -0800

    Corgi ASCII Added

--- Cpsc370Final/Corgi.cs
+++ Cpsc370Final/Corgi.cs
@@ -3,6 +3,10 @@ namespace Cpsc370Final;
 public class Corgi
 {
     // define corgi characters (using 2d array)
-    
+    string Corgi =
+        @" /\_/\ " + '\n' +
+        @"( •ᴥ• )" + '\n' +
+        @" ⊂  ⊂  " + '\n' +
+        @"  ∪  ∪ ";
     // handle corgi print
 }
