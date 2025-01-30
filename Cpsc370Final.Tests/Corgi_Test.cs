192e4682acd402e2cfd3b27a2036bdae830d2331
Kalin Richardson <kalrichardson@chapman.edu>
2025-01-29 20:31:59 -0800

    yay corgi

--- Cpsc370Final.Tests/Corgi_Test.cs
+++ Cpsc370Final.Tests/Corgi_Test.cs
@@ -1,6 +1,46 @@
 namespace Cpsc370Final.Tests;
 
+using xUnit;
+using System;
+
 public class Corgi_Test
 {
     // Format for naming is "WhatWeAreTestingAndActing_InputValue_AssertionAssumption
+    
+    [Fact]
+    public void Render_Corgi_ShouldHaveFourLines()
+    {
+        string[] corgiLines = Corgi.Split('\n');
+        Assert.Equal(4, corgiLines.Length);
+    }
+    
+    [Fact]
+    public void Map_InvalidPosition_ShouldThrowException()
+    {
+        Assert.Throws<NotImplementedException>(() => Map(10));
+    }
+    
+    [Fact]
+    public void Whack_Corgi_ShouldBeClearedFromBoard()
+    {
+        string boardBefore = Corgi;
+        string boardAfter = Empty;
+
+        Assert.NotEqual(boardBefore, boardAfter);
+    }
+
+    private (int, int) Map(int hole) =>
+        hole switch
+        {
+            1 => (06, 03),
+            2 => (20, 03),
+            3 => (34, 03),
+            4 => (06, 09),
+            5 => (20, 09),
+            6 => (34, 09),
+            7 => (06, 15),
+            8 => (20, 15),
+            9 => (34, 15),
+            _ => throw new NotImplementedException(),
+        };
 }
--- Cpsc370Final/Corgi.cs
+++ Cpsc370Final/Corgi.cs
@@ -3,10 +3,17 @@ namespace Cpsc370Final;
 public class Corgi
 {
     // define corgi characters (using 2d array)
-    string Corgi =
+    string Corgi { get; } =
         @" /\_/\ " + '\n' +
         @"( •ᴥ• )" + '\n' +
         @" ⊂  ⊂  " + '\n' +
         @"  ∪  ∪ ";
+
+    string Empty { get; } =
+        @"      " + '\n' +
+        @"      " + '\n' +
+        @"      " + '\n' +
+        @"      ";
+
     // handle corgi print
 }
