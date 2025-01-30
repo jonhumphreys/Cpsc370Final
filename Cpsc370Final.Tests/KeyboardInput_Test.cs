dc1162e3a86ae8a77c79dd9ca03167bab1f0a818
uimjpmh <121411948+uimjpmh@users.noreply.github.com>
2025-01-29 18:10:44 -0800

    Tests and Functions update in Keyboard Input
    Included 1 - 9 and Enter Keyboard input tests and all tests works fine

---
+++ Cpsc370Final.Tests/KeyboardInput_Test.cs
@@ -0,0 +1,45 @@
+﻿namespace Cpsc370Final.Tests;
+
+public class KeyboardInput_Test
+{
+    // Format for naming is "WhatWeAreTestingAndActing_InputValue_AssertionAssumption
+    [Theory]
+    [InlineData(ConsoleKey.D1, "D1 has been pressed")]
+    [InlineData(ConsoleKey.D2, "D2 has been pressed")]
+    [InlineData(ConsoleKey.D3, "D3 has been pressed")]
+    [InlineData(ConsoleKey.D4, "D4 has been pressed")]
+    [InlineData(ConsoleKey.D5, "D5 has been pressed")]
+    [InlineData(ConsoleKey.D6, "D6 has been pressed")]
+    [InlineData(ConsoleKey.D7, "D7 has been pressed")]
+    [InlineData(ConsoleKey.D8, "D8 has been pressed")]
+    [InlineData(ConsoleKey.D9, "D9 has been pressed")]
+    [InlineData(ConsoleKey.Enter, "Enter has been pressed")]
+    public void KeyboardInput_AllKeysUsedInGame_TriggersEachEvent(ConsoleKey userKeyInput, string expectedOutput)
+    {
+        Assert.Equal(KeyboardInput.getInstance().PrintKeyboardInput(userKeyInput), expectedOutput);
+    }
+
+    [Theory]
+    [InlineData( "1", "D1 has been pressed")]
+    [InlineData( "2", "D2 has been pressed")]
+    [InlineData( "3", "D3 has been pressed")]
+    [InlineData( "4", "D4 has been pressed")]
+    [InlineData( "5", "D5 has been pressed")]
+    [InlineData( "6", "D6 has been pressed")]
+    [InlineData( "7", "D7 has been pressed")]
+    [InlineData( "8", "D8 has been pressed")]
+    [InlineData( "9", "D9 has been pressed")]
+    [InlineData( "\r", "Enter has been pressed")]
+    public void KeyboardInputFromUser_AllKeysUsedInGame_TriggersEachEvent(string userConsoleInput, string expectedOutput)
+    {
+        // Simulate pressing "Enter" by providing input through StringReader
+        var input = new StringReader(userConsoleInput);  // Simulate the "Enter" key press
+        Console.SetIn(input);  // Redirect Console input
+
+        var keyboardInput = KeyboardInput.getInstance();
+        ConsoleKeyInfo userInput = keyboardInput.GetKeyboardInput(); // This will read the redirected input
+
+        Assert.Equal(expectedOutput, keyboardInput.PrintKeyboardInput(userInput.Key));
+        
+    }
+}   
---
+++ Cpsc370Final/KeyboardInput.cs
@@ -0,0 +1,54 @@
+﻿namespace Cpsc370Final;
+
+public class KeyboardInput
+{
+    private static KeyboardInput? instance;
+
+    public static KeyboardInput getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new KeyboardInput();
+        }
+        return instance;
+    }
+
+    private KeyboardInput()
+    {
+    }
+
+    public string PrintKeyboardInput(ConsoleKey userKeyInput)
+    {
+        if (userKeyInput == ConsoleKey.Enter)
+        {
+            return "Enter has been pressed";
+        }
+        else
+        {
+            return userKeyInput.ToString() + " has been pressed";
+        }
+    }
+
+    public ConsoleKeyInfo GetKeyboardInput()
+    {
+        // Read the next key as a character
+        char inputChar = (char)Console.Read();
+
+        // Map the input char to ConsoleKey
+        ConsoleKey key = ConsoleKey.NoName; // Default key
+
+        // Handle numeric keys (D1-D9)
+        if (inputChar >= '1' && inputChar <= '9')
+        {
+            key = (ConsoleKey)Enum.Parse(typeof(ConsoleKey), "D" + inputChar);
+        }
+        // Handle the Enter key
+        else if (inputChar == '\r') // Enter key is represented as \r (carriage return) in Console.Read()
+        {
+            key = ConsoleKey.Enter;
+        }
+
+        return new ConsoleKeyInfo(inputChar, key, false, false, false);
+    }
+    
+}
